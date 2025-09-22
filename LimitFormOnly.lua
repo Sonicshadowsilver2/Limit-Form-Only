26dc2f678f266398bc1402ec6a5c8fc07aa4c986
Sonicshadowsilver2 <danielctip2018@gmail.com>
2025-09-21 21:46:52 -0500

    Update LimitFormOnly.lua

--- LimitFormOnly.lua
+++ LimitFormOnly.lua
@@ -108,7 +108,6 @@ function _OnFrame()
 			WriteString(Obj0+0x0C8F0,'P_EX100_KH1F.mset\0') --Sora (Hunny Slider) MSET -> Limit Form MSET
 			WriteString(Obj0+0x12690,'P_EX100_KH1F\0\0\0\0\0') --Sora (On Carpet) Model -> Limit Form Model
 			WriteString(Obj0+0x154D0,'F_TT010_SORA.mset\0') --Skateboard (Roxas) MSET -> Skateboard (Sora) MSET
-			WriteString(Obj0+0x22E90,'P_EX100_KH1F\0\0\0') --Sora (Light Cycle) Model -> Limit Form Model
 		end
 	end
 end
